b59fe5b17644c5e7a9ddf2f9b8af33ded5a09680
Yavuz <akbay.yavuz@gmail.com>
2024-12-01 12:28:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='taxcore',
-    version='0.1.2',
+    version='0.1.4',
     description='A comprehensive technical analysis library for Python.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/ta-library',
+    url='https://github.com/YavuzAkbay/taxcore',
     author='Yavuz',
     author_email='akbay.yavuz@example.com',
     license='GPL-3.0',
